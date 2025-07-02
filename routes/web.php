831d254cb7f09b7bc388dae0f652152604f29e40
Zeeshan Rasool <zishan.rasool85@gmail.com>
2025-07-02 22:50:17 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => 'Laravel is working ✅');
 
-Route::middleware(['role:admin'])->group(function () {
-    Route::post('/wallet/debit', [WalletController::class, 'debit']);
-});
+
+//Route::middleware(['role:admin'])->group(function () {
+  //  Route::post('/wallet/debit', [WalletController::class, 'debit']);
+//});
